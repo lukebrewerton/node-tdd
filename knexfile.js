3acee9f422fadd9189f273d28dd250e79fd9b6fd
Luke Brewerton <luke.brewerton@netdimensions.com>
2019-02-15 22:05:02 +0000

    Initial Commit

---
+++ knexfile.js
@@ -0,0 +1,34 @@
+module.exports = {
+  test: {
+    client: "pg",
+    connection:
+      "postgres://node_user:node_password@localhost/mocha_chai_tv_shows_test",
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds/test"
+    }
+  },
+  development: {
+    client: "pg",
+    connection:
+      "postgres://node_user:node_password@localhost/mocha_chai_tv_shows",
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds/development"
+    }
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    },
+    seeds: {
+      directory: __dirname + "/db/seeds/production"
+    }
+  }
+};
